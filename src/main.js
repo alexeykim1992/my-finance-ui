fbe386efd142a9b14ae375f5a2c9e5a36d0e4c2d
Aleksey Kim <alexmail1992@gmail.com>
2021-09-30 22:45:01 +0500

    Add SCSS

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue';
 import App from '@/App';
+import components from '@/components/UI'
 import router from '@/pages/router';
 import store from "@/store";
+import '@/assets/style.scss';
 
 const app = createApp(App);
+
+components.forEach(component => {
+    app.component(component.name, component);
+})
 
 app
     .use(router)
