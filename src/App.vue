03b085d8c5d95d015c9b1c6a33116e5cce0452b6
Aleksey Kim <alexmail1992@gmail.com>
2021-10-01 20:54:38 +0500

    Add common styles

--- src/App.vue
+++ src/App.vue
@@ -12,19 +12,4 @@ export default {
 </script>
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: Verdana, serif;
-}
-
-a {
-  color: black;
-  text-decoration: none;
-}
-
-ul {
-  list-style: none;
-}
 </style>
