dde5791f528428a55c99b3855be21e394238937b
Aleksey Kim <alexmail1992@gmail.com>
2021-10-10 13:41:48 +0500

    Add today to AddTransactionDialog

--- src/store/dateModule.js
+++ src/store/dateModule.js
@@ -1,6 +1,6 @@
 export const dateModule = {
     getters: {
-        getDate: state => date => {
+        getDate: () => date => {
             return date.getFullYear().toString() + '-'
                 + (date.getMonth() + 1).toString().padStart(2, 0) + '-'
                 + date.getDate().toString().padStart(2, 0);
